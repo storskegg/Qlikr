58d0a5b3f5c37435b4d9ce191456d7962a21c4b1
Liam Conrad <liam@storskegg.org>
2020-02-29 23:33:00 -0600

    moar requirements

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # Update Pi and Python
 sudo apt-get update && sudo apt-get dist-upgrade -y
 
-sudo apt-get install python3-pip -y
+sudo apt-get install python3-pip libjpeg-dev zlib1g-dev libfreetype6-dev liblcms1-dev libopenjp2-7 libtiff5 -y
 
 sudo pip3 install --upgrade setuptools
 
@@ -16,4 +16,9 @@ sudo pip3 install adafruit-blinka
 sudo pip3 install adafruit-circuitpython-ssd1306
 sudo pip3 install adafruit-circuitpython-framebuf
 sudo pip3 install adafruit-circuitpython-rfm9x
-sudo pip3 install -U python-dotenv
+sudo pip3 install -U python-dotenv python-daemon
+
+# Pimoroni libs
+curl https://get.pimoroni.com/automationhat | bash
+
+# sudo pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo pip3 install -U
