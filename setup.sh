c0c13b94cdf3b915f91fcaf4f2af7708e9dea0fb
Liam Conrad <liam@storskegg.org>
2020-02-29 22:45:23 -0600

    fixes to setup

--- setup.sh
+++ setup.sh
@@ -10,8 +10,10 @@ sudo pip3 install --upgrade setuptools
 ls /dev/i2c* /dev/spi*
 
 # Install Python Libs
+sudo pip3 install -r requirements.txt
 sudo pip3 install RPI.GPIO
 sudo pip3 install adafruit-blinka
 sudo pip3 install adafruit-circuitpython-ssd1306
 sudo pip3 install adafruit-circuitpython-framebuf
 sudo pip3 install adafruit-circuitpython-rfm9x
+sudo pip3 install -U python-dotenv
